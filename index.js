c216cb6fc0fac9cda35792b84bbc3446b889cea9
tincho <martin.salinas@gmail.com>
2016-12-15 21:36:48 -0300

    getting functional

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * @TODO: if config.search_fields == '$all' (or some other wildcard)
+ * inspect the table and get all the fields
+ * var fieldNames;
+ * s.define(config.db_table).describe().then(function(fields) { fieldNames = Object.keys(fields)); });
+ */
+
 var config    = require('./config');
 var express   = require("express");
 var Sequelize = require('sequelize');
@@ -5,7 +12,6 @@ var sequelize = new Sequelize(config.db_name, config.db_user, config.db_password
   host: 'localhost',
   dialect: 'mysql',
 });
-
 
 var app = express();
 app.get("/search", function(req, res) {
@@ -20,14 +26,10 @@ app.get("/search", function(req, res) {
     var matchAllTerms = data.all;
     var terms = (matchFullTerm) ? [data.q] : data.q.replace(/\s+/g, ' ').split(' ');
 
-    var comparers = terms.map($likeMaker);
-    var inclusionType = ['$or', '$and'][+matchAllTerms];
     var conditions = {
-        '$or': config.search_fields.map(function(field) {
-            var condition = {};
-            condition[field] = {};
-            condition[field][inclusionType] = comparers;
-            return condition;
+        '$or': config.search_fields.map(fieldConditionsMaker, {
+            "conditions": terms.map($likeMaker),
+            "inclusionType": ['$or', '$and'][+matchAllTerms]
         })
     };
 
@@ -44,7 +46,13 @@ app.get("/search", function(req, res) {
         return {
             '$like': '%'+term+'%'
         };
-    }
+    };
+    function fieldConditionsMaker(field) {
+        var condition = {};
+        condition[field] = {};
+        condition[field][this.inclusionType] = this.conditions;
+        return condition;
+    };
 });
 
 app.listen(3000);
