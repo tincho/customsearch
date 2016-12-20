4627424b327bcd1520f4a688a597df4c98c9d9fd
tincho <martin.salinas@gmail.com>
2016-12-20 10:54:23 -0300

    improving(?!) configure.js

--- configure.js
+++ configure.js
@@ -1,13 +1,14 @@
 var readlineSync = require('readline-sync');
 var Sequelize    = require('sequelize');
-var _            = Sequelize.Utils._;
+var _            = Sequelize.Utils._; // give thanks and praises, hail to the _!
 var fs           = require('fs');
+
+var outputFile = "config.json.generated";
 
 var config = {
     db_driver: "mysql"
 };
-
-Object.assign(config, promptBasicAuth());
+_.assign(config, promptBasicAuth());
 
 var sequelize = new Sequelize(config.db_name, config.db_user, config.db_password, {
   host: config.db_host,
@@ -16,33 +17,51 @@ var sequelize = new Sequelize(config.db_name, config.db_user, config.db_password
 
 sequelize.showAllSchemas().then(
     _.flow(
+        // all first values are the table names:
         _.method('map', _.flow(_.values, _.first)),
         promptTable,
+        // once i get the answer set it to global var config
         _.partial(_.assign, config),
+        // and get it from there (config)
         _.flow(
             _.partial(_.get, _, "db_table"),
-            sequelize.define.bind(sequelize),
-            getColumns
+            // to run sequelize.define(config.db_table)
+            _.bind(sequelize.define, sequelize),
+            // and get its columns
+            _.method('describe'),
+            // which returns a promise so we call promise.then( fn )
+            _.method('then',
+                _.flow(
+                    // promise result is an object whose keys are the fields array
+                    Object.keys,
+                    // print them and ask to select search and display fields
+                    promptFields,
+                    // again result is set to config
+                    _.partial(_.assign, config),
+                    _.flow(
+                        JSON.stringify,
+                        _.partial(fs.writeFile, outputFile, _,
+                            (err) => ((err) ? console.log(err) : console.log("Config saved to " + outputFile))
+                        )
+                    )
+                )
+            )
         )
     )
 );
 
 function promptBasicAuth() {
     console.log('Please provide DB connection data. Only MySQL supported by now \n');
-
     var questions = {
         "db_name"     : { label: "DB name: " },
         "db_host"     : { label: "Host (empty: $<defaultInput>): ", options: { defaultInput: "localhost" } },
         "db_user"     : { label: "Username: " },
         "db_password" : { label: "Password: ", options: { hideEchoBack: true } }
     };
-    var answers = {};
-
-    Object.keys(questions).forEach(function(key) {
-        answers[key] = readlineSync.question(questions[key].label, questions[key].options || undefined);
-    });
-
-    return answers;
+    return  _.reduce(questions, function(answers, question, key) {
+        answers[key] = readlineSync.question(question.label, question.options || undefined);
+        return answers;
+    }, {});
 };
 
 function promptTable(tables) {
@@ -51,32 +70,12 @@ function promptTable(tables) {
     };
 };
 
-function getColumns(Model) {
-    Model.describe().then(
-        _.flow(
-            Object.keys,
-            promptFields,
-            _.partial(_.assign, config),
-            writeConfig
-        )
-    );
-}
-
 function promptFields(fields) {
     console.log('Columns are: ' + fields.join(', '));
-    var searchFields = readlineSync.question('Columns to search in (separated by "," or leave empty for all): ', { defaultInput: '*' });
+    var searchFields  = readlineSync.question('Columns to search in (separated by "," or leave empty for all): ', { defaultInput: '*' });
     var displayFields = readlineSync.question('Columns to return (separated by "," or leave empty for all): ', { defaultInput: '*' });
     return {
-        search_fields: (searchFields  !== '*') ?  searchFields.replace(' ', '').split(',') : searchFields,
+        search_fields:  (searchFields  !== '*') ?  searchFields.replace(' ', '').split(',') : searchFields,
         display_fields: (displayFields !== '*') ? displayFields.replace(' ', '').split(',') : displayFields,
     };
-}
-
-function writeConfig(config) {
-    fs.writeFile("config.json.generated", JSON.stringify(config), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("Config saved in config.json.generated");
-    });
 }
